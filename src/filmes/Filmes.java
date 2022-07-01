d7d3ac2ef41d5271a877eddddf1b822712eb4873
Leonardo Sanfins <leohsanfins@hotmail.com>
2022-06-30 22:54:21 -0300

    Primeiro commit

---
+++ src/filmes/Filmes.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filmes;
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import jdbc.PostgreSqlConnection;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Filmes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            JOptionPane.showMessageDialog(null, "Testando a conexão");
+            Connection con = new PostgreSqlConnection().conecta();
+            JOptionPane.showMessageDialog(null, "Está conectado");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar " + e);
+        }
+    }
+    
+}
---
+++ src/jdbc/PostgreSqlConnection.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author leonardo
+ */
+public class PostgreSqlConnection {
+    
+    public Connection conecta(){
+        
+        try {
+            
+            return DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/Filmes", "postgres", "postgres");
+        } catch (Exception erroSql) {
+            throw new RuntimeException(erroSql);
+        }
+    }
+}
